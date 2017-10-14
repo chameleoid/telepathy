053e39cfdc9ebf381888ffc1aad882db940f168b
rummik <k@9k1.us>
2017-10-14 06:56:54 -0400

    Add jshint to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const jshint = require('gulp-jshint');
 
 let source = {
   js: 'lib/*.js',
   test: 'test/*.js',
 };
 
-gulp.task('test', () =>
+gulp.task('test', [ 'jshint', 'mocha' ]);
+
+gulp.task('mocha', () =>
   gulp.src(source.test, { read: false })
     .pipe(mocha({ reporter: 'spec' }))
+);
+
+gulp.task('jshint', () =>
+  gulp.src([ source.js, source.test ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
 );
